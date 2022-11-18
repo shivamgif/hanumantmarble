dcb0727bf0d290f329e8933204155a1a71bd89af
shivamgif <52492676+shivamgif@users.noreply.github.com>
2022-11-18 14:54:39 +0100

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 
+import  NavBar from 'react-bootstrap/Navbar'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,54 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import React, { useState } from 'react';
 import '../styles/Home.module.css'
+import { Carousel, Nav,NavDropdown,Container, Navbar} from 'react-bootstrap'
+
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
   <div>
-  <header class="site-header sticky-top py-1">
-  <nav class="container d-flex flex-column flex-md-row justify-content-between">
-  <a class="py-0" href="#" aria-label="Product">
+  <Navbar class="site-header sticky-top py-1">
+  <Container>
+  <nav class="navbar navbar-expand-sm container d-flex flex-row justify-content-between">
+  <a class="py-0" href="/" aria-label="Product">
     <h2 class="mb-0">Hanumant Marble</h2>  
   </a>
-  <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-  <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-  <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-  <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-</nav>
-</header>
+
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+  <Nav className="me-auto">
+      <ul class="px-4 navbar-nav">
+        <Nav.Item class="py-2 px-4 "><a  href="#">Products</a></Nav.Item>
+        <Nav.Item class=" py-2 px-4 "><a href="#">Locations</a></Nav.Item>
+        <Nav.Item class="py-2 px-4 "><a  href="#">Get Your Quotation</a></Nav.Item>
+        <Nav.Item class="py-2 px-4 "><a  href="#">About</a></Nav.Item>
+        </ul>
+  </Nav>
+
+  </Navbar.Collapse>
+  </nav>
+  </Container>
+</Navbar>
 <main>
   <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
-  <div id="carouselExampleSlidesOnly" class="carousel slide customhero" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-    <img src="/hero1.png" class="d-block w-100 carimg"></img>
-    </div>
-    <div class="carousel-item">
-      <img src="/hero2.png" class="d-block w-100 carimg"></img>
-    </div>
-    <div class="carousel-item">
-      <img src="/hero3.png" class="d-block w-100 carimg"></img>
-    </div>
-  </div>
-  </div>
-    <div class="text-dark col-md-5 p-lg-5 mx-auto my-5 ">
+  <Carousel activeIndex={index} onSelect={handleSelect} class="customhero" controls={false} >
+    <Carousel.Item >
+      <img src="/hero1.png" class="d-block carimg"></img>
+    </Carousel.Item>
+    <Carousel.Item >
+      <img src="/hero2.jpeg" class="d-block carimg"></img>
+    </Carousel.Item>
+    <Carousel.Item >
+      <img src="/hero3.jpeg" class="d-block carimg"></img>
+    </Carousel.Item>
+  </Carousel>
+    <div class="text-light col-md-5 p-lg-5 mx-auto my-5 ">
       <h1 class="display-4 fw-normal">FOR YOUR SWEET HOME</h1>
       <p class="lead fw-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
       <a class="btn btn-outline-warning" href="#">Get Your Quotation</a>
@@ -41,14 +58,14 @@ export default function Home() {
   <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
     <div class="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
       <div class="my-3 py-3">
-        <h2 class="display-5">Another headline</h2>
+        <img src='/product0.png'></img>
         <p class="lead">And an even wittier subheading.</p>
       </div>
-      <div class="bg-light shadow-sm mx-auto custom-box" ></div>
+      <div class="bg-dark shadow-sm mx-auto custom-box" ></div>
     </div>
-    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
       <div class="my-3 p-3">
-        <h2 class="display-5">Another headline</h2>
+        <img src='/product1.png'></img>
         <p class="lead">And an even wittier subheading.</p>
       </div>
       <div class="bg-dark shadow-sm mx-auto custom-box" ></div>
@@ -56,99 +73,49 @@ export default function Home() {
   </div>
 
   <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="bg-light  me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
       <div class="my-3 p-3">
-        <h2 class="display-5">Another headline</h2>
+        <img src='/product2.png'></img>
         <p class="lead">And an even wittier subheading.</p>
       </div>
       <div class="bg-dark shadow-sm mx-auto custom-box"></div>
     </div>
-    <div class="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
       <div class="my-3 py-3">
-        <h2 class="display-5">Another headline</h2>
+      <img src='/product3.png'></img>
         <p class="lead">And an even wittier subheading.</p>
       </div>
       <div class="bg-light shadow-sm mx-auto custom-box"></div>
     </div>
   </div>
 
-  <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-      <div class="my-3 p-3">
-        <h2 class="display-5">Another headline</h2>
-        <p class="lead">And an even wittier subheading.</p>
-      </div>
-      <div class="bg-body shadow-sm mx-auto custom-box"></div>
-    </div>
-    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-      <div class="my-3 py-3">
-        <h2 class="display-5">Another headline</h2>
-        <p class="lead">And an even wittier subheading.</p>
-      </div>
-      <div class="bg-body shadow-sm mx-auto custom-box"></div>
-    </div>
-  </div>
 
-  <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-      <div class="my-3 p-3">
-        <h2 class="display-5">Another headline</h2>
-        <p class="lead">And an even wittier subheading.</p>
-      </div>
-      <div class="bg-body shadow-sm mx-auto custom-box" ></div>
-    </div>
-    <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden ">
-      <div class="my-3 py-3">
-        <h2 class="display-5">Another headline</h2>
-        <p class="lead">And an even wittier subheading.</p>
-      </div>
-      <div class="bg-body shadow-sm mx-auto custom-box"></div>
-    </div>
-  </div>
 </main>
 
 <footer class="container py-5">
   <div class="row">
     <div class="col-12 col-md">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Product</title><circle cx="12" cy="12" r="10"/><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+      <h4>Hanumant Marble</h4>
       <small class="d-block mb-3 text-muted">&copy; 2017–2022</small>
     </div>
     <div class="col-6 col-md">
       <h5>Features</h5>
       <ul class="list-unstyled text-small">
-        <li><a class="link-secondary" href="#">Cool stuff</a></li>
-        <li><a class="link-secondary" href="#">Random feature</a></li>
-        <li><a class="link-secondary" href="#">Team feature</a></li>
-        <li><a class="link-secondary" href="#">Stuff for developers</a></li>
-        <li><a class="link-secondary" href="#">Another one</a></li>
-        <li><a class="link-secondary" href="#">Last time</a></li>
       </ul>
     </div>
     <div class="col-6 col-md">
       <h5>Resources</h5>
       <ul class="list-unstyled text-small">
-        <li><a class="link-secondary" href="#">Resource name</a></li>
-        <li><a class="link-secondary" href="#">Resource</a></li>
-        <li><a class="link-secondary" href="#">Another resource</a></li>
-        <li><a class="link-secondary" href="#">Final resource</a></li>
       </ul>
     </div>
     <div class="col-6 col-md">
       <h5>Resources</h5>
       <ul class="list-unstyled text-small">
-        <li><a class="link-secondary" href="#">Business</a></li>
-        <li><a class="link-secondary" href="#">Education</a></li>
-        <li><a class="link-secondary" href="#">Government</a></li>
-        <li><a class="link-secondary" href="#">Gaming</a></li>
       </ul>
     </div>
     <div class="col-6 col-md">
       <h5>About</h5>
       <ul class="list-unstyled text-small">
-        <li><a class="link-secondary" href="#">Team</a></li>
-        <li><a class="link-secondary" href="#">Locations</a></li>
-        <li><a class="link-secondary" href="#">Privacy</a></li>
-        <li><a class="link-secondary" href="#">Terms</a></li>
       </ul>
     </div>
   </div>
